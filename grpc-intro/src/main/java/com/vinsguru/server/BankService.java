235bbe6cfd8bc32fd2cc8ad158cfcf76db8f2c42
t-raja <t.raja@olamnet.com>
2021-01-15 09:29:32 +0530

    Raja.T updated.

--- grpc-intro/src/main/java/com/vinsguru/server/BankService.java
+++ grpc-intro/src/main/java/com/vinsguru/server/BankService.java
@@ -1,8 +1,6 @@
 package com.vinsguru.server;
 
-import com.vinsguru.models.Balance;
-import com.vinsguru.models.BalanceCheckRequest;
-import com.vinsguru.models.BankServiceGrpc;
+import com.vinsguru.models.*;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -12,6 +10,25 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
         int accountNumber = request.getAccountNumber();
         Balance balance = Balance.newBuilder().setAmount(AccountDatabase.getBalance(accountNumber)).build();
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithDrawRequest request, StreamObserver<Money> responseObserver) {
+        int accountnumber= request.getAccountNumber();
+        int amount = request.getAmount();
+        int balance = AccountDatabase.getBalance(accountnumber);
+
+        for (int i = 0; i < amount/10 ; i++) {
+            Money money = Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(money);
+            AccountDatabase.detectBalance(accountnumber,10);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         responseObserver.onCompleted();
     }
 }
